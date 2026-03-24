fd450c6a30333bb7ce36f438116d9a092f197817
tmcarvalho <taniammc.94@gmail.com>
2026-03-24 18:58:38 +0100

    run kpis across all cores

--- algorithms/kpi_gen/ma_kpi_generator.py
+++ algorithms/kpi_gen/ma_kpi_generator.py
@@ -1,6 +1,7 @@
 import datetime
 
 import pandas as pd
+from joblib import Parallel, delayed
 from utils.constants import (
     DEFAULT_ITEM_COL,
     DEFAULT_TIMESTAMP_COL,
@@ -8,6 +9,27 @@ from utils.constants import (
 
 from algorithms.kpi_gen.indicators import *
 from algorithms.kpi_gen.kpi_generator import KPIGenerator
+
+
+def _compute_asset_kpis(asset_df, k, kpi_type):
+    """Compute all KPIs for a single asset. Called in parallel across assets."""
+    asset_df = asset_df.sort_values(by=DEFAULT_TIMESTAMP_COL, ascending=True)
+    periods_3 = [21, 63, 126] if kpi_type == "short" else [21, 63, 126, 189]
+    periods_4 = [1, 21, 63, 126] if kpi_type == "short" else [1, 21, 63, 126, 189]
+    asset_df = avg_price(asset_df, periods=periods_3)
+    asset_df = roi(asset_df, periods=periods_4)
+    asset_df = volatility(asset_df, periods=periods_3)
+    asset_df = moving_average_convergence_divergence(asset_df)
+    asset_df = momentum(asset_df, periods=periods_3)
+    asset_df = rate_of_change(asset_df, periods=periods_3)
+    asset_df = relative_strength_index(asset_df)
+    asset_df = detrended_close_oscillator(asset_df)
+    asset_df = sharpe(asset_df, periods=periods_3)
+    asset_df = min_max(asset_df, periods=periods_3)
+    for column in asset_df.columns:
+        if column != DEFAULT_ITEM_COL and column != DEFAULT_TIMESTAMP_COL:
+            asset_df[column] = asset_df[column].rolling(k).mean()
+    return asset_df.dropna()
 
 
 class MAKPIGenerator(KPIGenerator):
@@ -27,50 +49,16 @@ class MAKPIGenerator(KPIGenerator):
         Computes the desired KPIs.
         :return: a dataframe containing the KPIs.
         """
-
         timea = datetime.datetime.now()
-        assets = self.data[DEFAULT_ITEM_COL].unique()
-        asset_dfs = []
-        # Step 2: For each asset:
-        j = 0
-        for asset in assets:
-            # b) Now, we add it to a pandas DataFrame
+        asset_groups = [grp for _, grp in self.data.groupby(DEFAULT_ITEM_COL, sort=False)]
 
-            asset_time_series_df = self.data[self.data[DEFAULT_ITEM_COL] == asset]
-            asset_time_series_df = asset_time_series_df.sort_values(by=DEFAULT_TIMESTAMP_COL, ascending=True)
+        asset_dfs = Parallel(n_jobs=-1)(
+            delayed(_compute_asset_kpis)(grp, self.k, self.kpi_type)
+            for grp in asset_groups
+        )
 
-            # b) Compute the technical indicators:
-            asset_time_series_df = avg_price(asset_time_series_df, periods=([21, 63, 126] if self.kpi_type == "short"
-                                                                            else [21, 63, 126, 189]))
-            asset_time_series_df = roi(asset_time_series_df, periods=([1, 21, 63, 126] if self.kpi_type == "short"
-                                                                    else [1, 21, 63, 126, 189]))
-            asset_time_series_df = volatility(asset_time_series_df, periods=([21, 63, 126] if self.kpi_type == "short"
-                                                                            else [21, 63, 126, 189]))
-            asset_time_series_df = moving_average_convergence_divergence(asset_time_series_df)
-            asset_time_series_df = momentum(asset_time_series_df, periods=([21, 63, 126] if self.kpi_type == "short"
-                                                                        else [21, 63, 126, 189]))
-            asset_time_series_df = rate_of_change(asset_time_series_df, periods=([21, 63, 126] if self.kpi_type == "short"
-                                                                                else [21, 63, 126, 189]))
-            asset_time_series_df = relative_strength_index(asset_time_series_df)
-            asset_time_series_df = detrended_close_oscillator(asset_time_series_df)
-            asset_time_series_df = sharpe(asset_time_series_df, periods=([21, 63, 126] if self.kpi_type == "short"
-                                                                        else [21, 63, 126, 189]))
-            asset_time_series_df = min_max(asset_time_series_df, periods=([21, 63, 126] if self.kpi_type == "short"
-                                                                        else [21, 63, 126, 189]))
-            #asset_time_series_df = asset_time_series_df.dropna()
-
-            for column in asset_time_series_df.columns:
-                if column != DEFAULT_ITEM_COL and column != DEFAULT_TIMESTAMP_COL:
-                    asset_time_series_df[column] = asset_time_series_df[column].rolling(self.k).mean()
-            asset_time_series_df = asset_time_series_df.dropna()
-
-            asset_dfs.append(asset_time_series_df)
-
-            j += 1
-            if j % 100 == 0:
-                string = "Generated the indicators for " + str(j) + " assets ("
-                time_elapsed = datetime.datetime.now() - timea
-                print(string + '{}'.format(time_elapsed) + ")")
+        time_elapsed = datetime.datetime.now() - timea
+        print(f"Generated indicators for {len(asset_dfs)} assets ({time_elapsed})")
 
         full_df = pd.concat(asset_dfs)
         self.kpis = full_df
